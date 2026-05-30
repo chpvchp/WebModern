a25422b1cebe4c2e88e55ca1e69d9143b2891937
chpvchp <caohoangphuc025@gmail.com>
2026-05-30 22:31:38 +0700

    done exam-1 level-1 rewrite updated!

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
-class Todo(BaseModel):
+class ToDo(BaseModel):
     title: str
 
 app = FastAPI()
@@ -14,27 +14,27 @@ app.add_middleware(
 )
 
 list_todos = []
-id_title = 0
+id_todo = 0
 
 @app.get("/todos")
-def get_todos():
+def get_list_todo():
     global list_todos
     return list_todos
 
 @app.post("/todos")
-def post_todos(todo: Todo):
-    global list_todos
-    global id_title
-    id_title += 1
+def add_todo(todo: ToDo):
+    global list_todos, id_todo
     new_todo = {
-        "id": id_title,
+        "id": id_todo,
         "title": todo.title
     }
+    id_todo += 1
     list_todos.append(new_todo)
-    return list_todos
+    return {"message": "added!"}
 
 @app.delete("/todos/{todo_id}")
 def delete_todo(todo_id: int):
     global list_todos
+    
     list_todos = [todo for todo in list_todos if todo["id"] != todo_id]
-    return {"message": "deleted"}
+    return {"message": "deleted!"}
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,22 +2,25 @@ import { useEffect, useState } from "react";
 
 export default function App() {
   const [text, setText] = useState("");
-  const [todos, setTodos] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [todos, setTodo] = useState([]);
   const [adding, setAdding] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [deleting, setDeleting] = useState(false);
 
   const getTodos = async () => {
     setLoading(true);
     const res = await fetch("http://localhost:8000/todos");
     const data = await res.json();
-    setTodos(data);
+    setTodo(data);
     setLoading(false);
   };
 
   const addTodo = async () => {
     setAdding(true);
-    if (!text.trim()) return;
-
+    if (!text.trim()) {
+      setAdding(false);
+      return;
+    }
     await fetch("http://localhost:8000/todos", {
       method: "POST",
       headers: {
@@ -25,17 +28,18 @@ export default function App() {
       },
       body: JSON.stringify({ title: text }),
     });
-
     setText("");
     getTodos();
     setAdding(false);
   };
 
   const deleteTodo = async (id) => {
+    setDeleting(true);
     await fetch(`http://localhost:8000/todos/${id}`, {
       method: "DELETE",
     });
     getTodos();
+    setDeleting(false);
   };
 
   useEffect(() => {
@@ -43,35 +47,36 @@ export default function App() {
   }, []);
 
   return (
-    <div className="min-h-screen m-0 p-0 flex flex-col justify-center items-center bg-gray-100">
-      <div className="p-4 bg-white flex flex-col rounded-lg gap-4 shadow-lg">
+    <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center gap-2">
+      <div className="p-4 bg-white border border-gray-300 rounded-xl shadow-lg flex flex-col gap-2">
         <textarea
-          className="p-4 border rounded-lg"
+          className="p-4 border border-gray-400 rounded-lg transition duration-200 hover:border-gray-600 resize-none text-center"
+          placeholder="Nhập title todo..."
           value={text}
           onChange={(e) => setText(e.target.value)}
-          placeholder="Nhập title todo..."
         ></textarea>
         <button
-          className={`p-2 rounded-lg text-white font-sans font-bold transition duration-200
-            ${adding ? "bg-gray-400 cursor-not-allowed" : "bg-blue-400 hover:bg-blue-600"}`}
+          className={`p-2 text-white font-sans font-bold rounded-lg ${adding ? "bg-gray-400 cursor-not-allowed" : "bg-blue-400 transition duration-200 hover:bg-blue-600"}`}
           onClick={addTodo}
           disabled={adding}
         >
-          {adding ? "Adding.." : "Add Todo"}
+          {adding ? "Adding..." : "Add Todo"}
         </button>
       </div>
-      <div className="mt-8 flex gap-4">
+      <div className="mt-4 flex gap-4">
+        {/* <p className="m-2 text-center italic">Loading...</p> */}
+
         {loading ? (
-          <p className="text-center italic">Loading...</p>
+          <p className="m-2 text-center italic">Loading...</p>
         ) : (
           todos.map((todo) => (
-            <div className="p-4 flex flex-col rounded-lg bg-white shadow justify-center items-center" key={todo.id}>
-              <p className="p-2 text-center border rounded-lg">{todo.title}</p>
+            <div className="p-4 bg-white rounded-lg flex flex-col justify-center items-center gap-2 shadow transition duration-200 hover:scale-105 hover:shadow-md" key={todo.id}>
+              <p className="p-2 border rounded-md text-center">{todo.title}</p>
               <button
-                className="mt-2 p-2 rounded-lg bg-blue-400 text-white font-bold font-sans transition duration-200 hover:bg-blue-600"
+                className={`p-2 text-white font-sans font-bold rounded-md ${deleting ? "bg-gray-400 cursor-not-allowed" : " bg-blue-400 transition duration-200 hover:bg-blue-600 "}`}
                 onClick={() => deleteTodo(todo.id)}
               >
-                Delete
+                {deleting ? "Del..." : "Delete"}
               </button>
             </div>
           ))
